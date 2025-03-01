041f47930f9147617bfcbdcaa1429a44b1d60e7d
CDavidDv <carlos.david1806@gmail.com>
2025-03-01 13:48:22 -0600

    print data

--- resources/js/Components/Caja.vue
+++ resources/js/Components/Caja.vue
@@ -276,9 +276,9 @@ const procesarPago = () => {
         icon: 'success',
         title: 'Pago procesado con éxito.',
       });
+      printTicket(selectedOrderId.value)
       ordenes.value = response.props.ordenes || [];
       recentTickets.value = response.props.ordenesRecientes || [];
-      printTicket(selectedOrderId.value)
     },
     onError: (error) => {
       console.error('Error al procesar el pago:', error);
@@ -396,6 +396,8 @@ window.Echo.channel(`pedidos_sucursal_${sucursalId}`)
 
 
   const printTicket = (id) => {
+
+    console.log("El id de ticket")
     console.log("El id de ticket",id)
   const order = ordenes.value.find(order => order.id === id) || props.ordenesRecientes.find(orden => orden.id === id); // Encuentra el pedido por su ID
   
