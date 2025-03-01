3b70142f961ff061b549e4090e874bc391a7ebaa
CDavidDv <carlos.david1806@gmail.com>
2025-03-01 13:50:35 -0600

    print ticket

--- resources/js/Components/Caja.vue
+++ resources/js/Components/Caja.vue
@@ -261,6 +261,7 @@ const procesarPago = () => {
   }
 
   loading.value = true;
+  printTicket(selectedOrderId.value)
   
   router.post('/terminar_pedido', {
     id: selectedOrderId.value,
@@ -276,7 +277,6 @@ const procesarPago = () => {
         icon: 'success',
         title: 'Pago procesado con éxito.',
       });
-      printTicket(selectedOrderId.value)
       ordenes.value = response.props.ordenes || [];
       recentTickets.value = response.props.ordenesRecientes || [];
     },
